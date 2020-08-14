4fd4c860369460e01bb988c4f1cff59205a9de8c
Anthony Almeida <anthony.almeida@chipinside.com.br>
2020-08-13 23:10:46 -0300

    21/22. First components with styled-components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import * as S from './styles'
 
-export default function Main() {
-  return <h1>React avançado</h1>
-}
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e react Avançado escriot ao lado."
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para um computador"
+    />
+  </S.Wrapper>
+)
+
+export default Main
